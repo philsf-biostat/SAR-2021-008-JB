c967ed34b0a76b36e4afad29c4af5c07f4bfd59b
Felipe Figueiredo <philsf79@gmail.com>
2021-11-17 23:26:22 -0300

    plot df movido para script plots

--- scripts/inference.R
+++ scripts/inference.R
@@ -1,15 +1,6 @@
 # setup -------------------------------------------------------------------
 library(survminer)
 
-# newdat <- expand.grid(sexo = levels(analytical$sexo), paricoes = as.character(0:1))[-3, ]
-# cxsf <- survfit(m.sexo.par, data = analytical, conf.type = "plain", newdata = newdat)
-
-surv_cxsf <- surv_summary(cxsf, data = analytical) %>% tibble()
-m_newdat <- newdat[as.character(surv_cxsf$strata), ]
-
-## plotting data frame
-surv_df <- cbind(surv_cxsf, m_newdat)
-
 # tables ------------------------------------------------------------------
 
 tab_inf <- tbl_survfit(
